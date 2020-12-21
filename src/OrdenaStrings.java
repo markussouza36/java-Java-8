8dd0d5be0635463be8360cc8adf644f60a1236ff
markussouza36 <72872451+markussouza36@users.noreply.github.com>
2020-12-21 16:39:13 -0300

    Add files via upload

---
+++ src/OrdenaStrings.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class OrdenaStrings {
+	public static void main(String[] args) {
+
+		List<String> palavras = new ArrayList<>();
+		palavras.add("alura online");
+		palavras.add("editora casa do codigo");
+		palavras.add("caelum");
+
+		palavras.sort((s1, s2) -> {
+			if (s1.length() < s2.length())
+				return -1;
+			if (s1.length() > s2.length())
+				return 1;
+			return 0;
+		});
+
+		//palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		
+		palavras.sort(Comparator.comparing(s -> s.length()));
+		palavras.sort(Comparator.comparing(String::length));
+		
+		Function<String, Integer> funcao = String::length;
+		Function<String, Integer> funcao2 = s -> s.length();
+		
+		Comparator<String> comparador = Comparator.comparing(funcao);
+		palavras.sort(comparador);
+
+		System.out.println(palavras);
+		
+		Consumer<String> impressor = System.out::println;
+		palavras.forEach(impressor);
+
+		palavras.forEach(System.out::println);
+		
+		palavras.sort(String.CASE_INSENSITIVE_ORDER);
+		
+	}
+}
+
+class ComparadorPorTamanho implements Comparator<String> {
+
+	@Override
+	public int compare(String s1, String s2) {
+		if (s1.length() < s2.length())
+			return -1;
+		if (s1.length() > s2.length())
+			return 1;
+		return 0;
+	}
+
+}
